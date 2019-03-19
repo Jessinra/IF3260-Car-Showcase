8160721799eaaa8f091937641f1f7536d17fab77
Joseph Salimin <13516037@std.stei.itb.ac.id>
2019-03-19 19:36:02 +0700

    add initial first week

---
+++ first-week/main.cpp
@@ -0,0 +1,28 @@
+// #include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main(int argc, char *argv[]) {
+    GLFWwindow* window;
+
+    if (!glfwInit()) {
+        return -1;
+    }
+
+    window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+    
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
