d08de0221a51af955d564046728c672a83f88f76
Sammy <13516103@std.stei.itb.ac.id>
2019-03-26 10:15:44 +0000

    Use OpenGL 3.3
    mesa on f29 doesn't yet support OpenGL 4.0, and we only use 3.3 APIs anyway

--- second-week-a/lib/Window.cpp
+++ second-week-a/lib/Window.cpp
@@ -14,8 +14,8 @@ Window::Window(int width, int height, const char* title) : title(title) {
 
 void Window::initializeGLFW() {
   glfwInit();
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 }
 
