55f481d0d719a4a007e940bde37f0e64cd95048d
Joseph Salimin <13516037@std.stei.itb.ac.id>
2019-04-09 22:43:11 +0700

    change location

--- third-week-b/main.cpp
+++ third-week-b/main.cpp
@@ -143,7 +143,7 @@ void setupWindow(GLFWwindow **window){
     }
 
     glfwSetFramebufferSizeCallback(*window, framebuffer_size_callback);
-    glfwSetCursorPosCallback(*window, mouseCallback);
+    // glfwSetCursorPosCallback(*window, mouseCallback);
     glfwMakeContextCurrent(*window);
 }
 
