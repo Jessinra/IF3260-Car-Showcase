72605fd5add61a4b0c3765687549d1a53f64eff2
Sammy <13516103@std.stei.itb.ac.id>
2019-05-01 23:17:27 +0000

    Use ballRand() for more natural smoke distribution

--- fifth-week/objects/SmokeGenerator.cpp
+++ fifth-week/objects/SmokeGenerator.cpp
@@ -1,31 +1,13 @@
 #include "SmokeGenerator.hpp"
-#include <GLFW/glfw3.h>
+#include <glm/gtc/random.hpp>
 
 SmokeGenerator::SmokeGenerator() {
-    std::random_device rd;           // obtain a random number from hardware
-    this->eng = std::mt19937(rd());  // seed the generator
-
-    this->randomX = std::uniform_int_distribution<>(-230, -200);  // define the range
-    this->randomY = std::uniform_int_distribution<>(-150, -100);  // define the range
-    this->randomZ = std::uniform_int_distribution<>(-200, -100);  // define the range
-
-    this->randomVX = std::uniform_int_distribution<>(-500, -100);  // define the range
-    this->randomVY = std::uniform_int_distribution<>(-100, 300);   // define the range
-    this->randomVZ = std::uniform_int_distribution<>(-100, 100);   // define the range
 }
 
 glm::vec3 SmokeGenerator::getRandomPosition() {
-    float X = randomX(eng) / 100.0;
-    float Y = randomY(eng) / 100.0;
-    float Z = randomZ(eng) / 100.0;
-
-    return glm::vec3(X, Y, Z);
+    return glm::vec3(-2.15, -1.25, -1.5) + glm::ballRand(0.25f);
 }
 
 glm::vec3 SmokeGenerator::getRandomVelocity() {
-    float VX = randomVX(eng) / 1000.0;
-    float VY = randomVY(eng) / 1000.0;
-    float VZ = randomVZ(eng) / 1000.0;
-
-    return glm::vec3(VX, VY, VZ);
+    return glm::vec3(-0.3, 0.1, 0.0) + glm::ballRand(0.2f);
 }
--- fifth-week/objects/SmokeGenerator.hpp
+++ fifth-week/objects/SmokeGenerator.hpp
@@ -1,27 +1,13 @@
 #ifndef SMOKE_GENERATOR_HPP
 #define SMOKE_GENERATOR_HPP
 
-#include <iostream>
-#include <random>
-
 #include "Particles.hpp"
 
 class SmokeGenerator {
-   private:
-
-    std::mt19937 eng;  
-    std::uniform_int_distribution<> randomX;  
-    std::uniform_int_distribution<> randomY;  
-    std::uniform_int_distribution<> randomZ;  
-
-    std::uniform_int_distribution<> randomVX;  
-    std::uniform_int_distribution<> randomVY;   
-    std::uniform_int_distribution<> randomVZ;   
    public:
     SmokeGenerator();
     glm::vec3 getRandomPosition();
-    glm::vec3 getRandomVelocity();
-    
+    glm::vec3 getRandomVelocity();    
 };
 
 #endif
